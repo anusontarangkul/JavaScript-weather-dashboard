20e570c41d5b52cd44bcc684de55737aaaac8a6c
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-09-28 17:39:30 -0700

    add pseudo code

---
+++ app.js
@@ -0,0 +1,15 @@
+var APIkey = "54f5d77c61f7b7d7da7d2c41a2956900";
+
+// search for city button
+// get current and future conditions
+//append results to search history
+
+//click current city
+//get details with city name, date, icon, temp, humidity, wind speed, and UV, index
+//color to show conditions are favorable, moderate, or severe
+
+//click future weather conditiosn,
+// get 5 day forecast that displays date, icon ,temperature, and humidity
+
+//click on search history
+//get current and future as well
