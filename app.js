0848a66797d20465dbb3bc529f05b1e270806ef7
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-09-28 19:15:00 -0700

    update search function

--- app.js
+++ app.js
@@ -17,72 +17,72 @@ var APIkey = "54f5d77c61f7b7d7da7d2c41a2956900";
 $(document).ready(function () {
   $("#search-button").on("click", function () {
     var searchValue = $("#search-value").val();
+
     // clear input box
     // $("#search-value").val("");
 
-    searchWeather(searchValue);
+    // searchWeather(searchValue);
   });
 
-  function searchWeather(searchValue) {
-    var queryURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      searchValue +
-      "&appid=54f5d77c61f7b7d7da7d2c41a2956900";
-    $.ajax({
-      type: "GET",
-      url: queryURL,
-    }).then(function (response) {
-      console.log(response);
-    });
-    // $.ajax({
-    //     type: "GET",
-    //     url:
-    //       "http://api.openweathermap.org/data/2.5/weather?q=" +
-    //       searchValue +
-    //       "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial",
-    //     dataType: "json",
-    //     success: function (data) {
-    //       // create history link for this search
-    //       if (history.indexOf(searchValue) === -1) {
-    //         history.push(searchValue);
-    //         window.localStorage.setItem("history", JSON.stringify(history));
+  // function searchWeather(searchValue) {
+  //   var queryURL =
+  //     "https://api.openweathermap.org/data/2.5/weather?q=" +
+  //     searchValue +
+  //     "&appid=54f5d77c61f7b7d7da7d2c41a2956900";
+  //   $.ajax({
+  //     type: "GET",
+  //     url: queryURL,
+  //   }).then(function (response) {
+  //     console.log(response);
+  //   });
+  // $.ajax({
+  //     type: "GET",
+  //     url:
+  //       "http://api.openweathermap.org/data/2.5/weather?q=" +
+  //       searchValue +
+  //       "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial",
+  //     dataType: "json",
+  //     success: function (data) {
+  //       // create history link for this search
+  //       if (history.indexOf(searchValue) === -1) {
+  //         history.push(searchValue);
+  //         window.localStorage.setItem("history", JSON.stringify(history));
 
-    //         makeRow(searchValue);
-    //       }
+  //         makeRow(searchValue);
+  //       }
 
-    //       // clear any old content
-    //       $("#today").empty();
+  //       // clear any old content
+  //       $("#today").empty();
 
-    //       // create html content for current weather
-    //       var title = $("<h3>")
-    //         .addClass("card-title")
-    //         .text(data.name + " (" + new Date().toLocaleDateString() + ")");
-    //       var card = $("<div>").addClass("card");
-    //       var wind = $("<p>")
-    //         .addClass("card-text")
-    //         .text("Wind Speed: " + data.wind.speed + " MPH");
-    //       var humid = $("<p>")
-    //         .addClass("card-text")
-    //         .text("Humidity: " + data.main.humidity + "%");
-    //       var temp = $("<p>")
-    //         .addClass("card-text")
-    //         .text("Temperature: " + data.main.temp + " °F");
-    //       var cardBody = $("<div>").addClass("card-body");
-    //       var img = $("<img>").attr(
-    //         "src",
-    //         "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
-    //       );
+  //       // create html content for current weather
+  //       var title = $("<h3>")
+  //         .addClass("card-title")
+  //         .text(data.name + " (" + new Date().toLocaleDateString() + ")");
+  //       var card = $("<div>").addClass("card");
+  //       var wind = $("<p>")
+  //         .addClass("card-text")
+  //         .text("Wind Speed: " + data.wind.speed + " MPH");
+  //       var humid = $("<p>")
+  //         .addClass("card-text")
+  //         .text("Humidity: " + data.main.humidity + "%");
+  //       var temp = $("<p>")
+  //         .addClass("card-text")
+  //         .text("Temperature: " + data.main.temp + " °F");
+  //       var cardBody = $("<div>").addClass("card-body");
+  //       var img = $("<img>").attr(
+  //         "src",
+  //         "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+  //       );
 
-    //       // merge and add to page
-    //       title.append(img);
-    //       cardBody.append(title, temp, humid, wind);
-    //       card.append(cardBody);
-    //       $("#today").append(card);
+  //       // merge and add to page
+  //       title.append(img);
+  //       cardBody.append(title, temp, humid, wind);
+  //       card.append(cardBody);
+  //       $("#today").append(card);
 
-    //       // call follow-up api endpoints
-    //       getForecast(searchValue);
-    //       getUVIndex(data.coord.lat, data.coord.lon);
-    //     },
-    //   });
-  }
+  //       // call follow-up api endpoints
+  //       getForecast(searchValue);
+  //       getUVIndex(data.coord.lat, data.coord.lon);
+  //     },
+  //   });
 });
